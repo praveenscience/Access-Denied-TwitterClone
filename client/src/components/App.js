52c8c2cf968832e86d6f5d873937af1181ce1353
Praveen Kumar <praveenscience@gmail.com>
2021-03-18 13:35:31 +0000

    Implemented a simple header.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Access Denied</h1>
+        <Header dark={true}>React Twitter</Header>
       </div>
     );
   }
